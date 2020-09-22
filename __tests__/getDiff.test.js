d4c5be18c39b8428e93045cd23f54fc199e6637c
eifory <bartem007@gmail.com>
2020-09-22 14:01:31 +0300

    add ini parser

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -4,8 +4,8 @@ import { dirname, join } from 'path';
 import getDiff from '../index.js';
 
 describe('json files diff result', () => {
-  const beforePath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'file1.json');
-  const afterPath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'file2.json');
+  const beforePath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'before.json');
+  const afterPath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'after.json');
   const jsonResultPath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'genDiffResult');
 
   it('should return string', () => {
@@ -21,8 +21,25 @@ describe('json files diff result', () => {
 });
 
 describe('yml files diff result', () => {
-  const beforePath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'file1.yml');
-  const afterPath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'file2.yml');
+  const beforePath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'before.yml');
+  const afterPath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'after.yml');
+  const jsonResultPath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'genDiffResult');
+
+  it('should return string', () => {
+    const result = getDiff(beforePath, afterPath);
+    expect(typeof result).toBe('string');
+  });
+
+  it('should return result as in genDiffResult', () => {
+    const result = getDiff(beforePath, afterPath);
+    const expected = fs.readFileSync(jsonResultPath, 'utf8');
+    expect(result).toBe(expected);
+  });
+});
+
+describe('ini files diff result', () => {
+  const beforePath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'before.ini');
+  const afterPath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'after.ini');
   const jsonResultPath = join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', 'genDiffResult');
 
   it('should return string', () => {
