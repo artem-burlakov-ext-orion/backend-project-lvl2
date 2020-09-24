9cdb6781d370a4d6fa38e923a4989c2a69ac90e9
eifory <bartem007@gmail.com>
2020-09-24 16:08:21 +0300

    fix index

--- index.js
+++ index.js
@@ -9,38 +9,42 @@ const isAdded = (before, after, key) => _.has(after, key) && !_.has(before, key)
 const isChanged = (before, after, key) => isExist(before, after, key) && before[key] !== after[key];
 const isObject = (before, after, key) => _.isObject(before[key]) && _.isObject(after[key]);
 
-const getStates = (before, after, key, f) => ({
-  added: {
+const getStates = (before, after, key, f) => [
+  {
+    state: 'added',
     check: () => isAdded(before, after, key),
     result: (depth) => `${' '.repeat(depth)}+ ${key}: ${after[key]}`,
   },
-  deleted: {
+  {
+    state: 'deleted',
     check: () => isDeleted(before, after, key),
     result: (depth) => `${' '.repeat(depth)}- ${key}: ${before[key]}`,
   },
-  unchanged: {
+  {
+    state: 'unchanged',
     check: () => isUnchanged(before, after, key),
     result: (depth) => `${' '.repeat(depth)}  ${key}: ${before[key]}`,
   },
-  changed: {
+  {
+    state: 'changed',
     check: () => isChanged(before, after, key),
     result: (depth) => `${' '.repeat(depth)}- ${key}: ${before[key]}\n${' '.repeat(depth)}+ ${key}: ${after[key]}`,
   },
-  nested: {
+  {
+    state: 'nested',
     check: () => isObject(before, after, key),
     result: (depth) => f(before[key], after[key], depth + 2),
   }
-});
+];
 
- export default (firstFilePath, secondFilePath) => {
+export default (firstFilePath, secondFilePath) => {
   const before = readFile(firstFilePath);
   const after = readFile(secondFilePath);
 
   const f = (before, after, depth) => {
     const uniqKeys = [...new Set([...Object.keys(before), ...Object.keys(after)])];
     const allData = uniqKeys.reduce((acc, key) => {
-      const [, { result }] = Object.entries(getStates(before, after, key, f))
-        .find(([, stateVal]) => stateVal.check());
+      const { result } = getStates(before, after, key, f).find(({ check }) => check());
       acc.push(result(depth));
       return acc;
     }, []);
@@ -48,3 +52,20 @@ const getStates = (before, after, key, f) => ({
   }
   return f(before, after, 2);
 };
+
+//  export default (firstFilePath, secondFilePath) => {
+//   const before = readFile(firstFilePath);
+//   const after = readFile(secondFilePath);
+
+//   const f = (before, after, depth) => {
+//     const uniqKeys = [...new Set([...Object.keys(before), ...Object.keys(after)])];
+//     const allData = uniqKeys.reduce((acc, key) => {
+//       const [, { result }] = Object.entries(getStates(before, after, key, f))
+//         .find(([, stateVal]) => stateVal.check());
+//       acc.push(result(depth));
+//       return acc;
+//     }, []);
+//     return `{\n${allData.join('\n')}\n}`;
+//   }
+//   return f(before, after, 2);
+// };
