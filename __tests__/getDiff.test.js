88cf80f767621ff8acc4aeb0666dca1417bd310d
eifory <bartem007@gmail.com>
2020-10-14 20:23:49 +0300

    fix test

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -5,23 +5,30 @@ import buildDiff from '../index.js';
 
 const getPath = (fileName) => join(dirname(fileURLToPath(import.meta.url)), '..', '__fixtures__', fileName);
 
-const inputExts = ['ini', 'yml', 'json'];
-const outputFormats = ['stylish', 'plain', 'json'];
+const testData = [
+  ['yml', 'yml', 'stylish'],
+  ['yml', 'yml', 'plain'],
+  ['yml', 'yml', 'json'],
+  ['yml', 'ini', 'stylish'],
+  ['yml', 'ini', 'plain'],
+  ['yml', 'ini', 'json'],
+  ['yml', 'json', 'stylish'],
+  ['yml', 'json', 'plain'],
+  ['yml', 'json', 'json'],
+  ['ini', 'ini', 'stylish'],
+  ['ini', 'ini', 'plain'],
+  ['ini', 'ini', 'json'],
+  ['ini', 'json', 'stylish'],
+  ['ini', 'json', 'plain'],
+  ['ini', 'json', 'json'],
+  ['json', 'json', 'stylish'],
+  ['json', 'json', 'plain'],
+  ['json', 'json', 'json'],
+];
 
-const testData = outputFormats.reduce((acc, format) => {
-  const resultPath = getPath(`result.${format}`);
-  let testDataCurrentValue = [...acc];
-  inputExts.forEach((beforeExt) => {
-    inputExts.forEach((afterExt) => {
-      const beforePath = getPath(`before-nested.${beforeExt}`);
-      const afterPath = getPath(`after-nested.${afterExt}`);
-      testDataCurrentValue = [...testDataCurrentValue, [beforePath, afterPath, format, resultPath]];
-    });
-  });
-  return testDataCurrentValue;
-}, []);
-
-test.each(testData)('before: %s\nafter: %s\noutput: %s', (before, after, format, result) => {
-  const expected = fs.readFileSync(result, 'utf8');
-  expect(buildDiff(before, after, format)).toBe(expected);
+test.each(testData)('before: %s\nafter: %s\noutput: %s', (beforeExt, afterExt, format) => {
+  const expected = fs.readFileSync(getPath(`result.${format}`), 'utf8');
+  const beforePath = getPath(`before-nested.${beforeExt}`);
+  const afterPath = getPath(`after-nested.${afterExt}`);
+  expect(buildDiff(beforePath, afterPath, format)).toBe(expected);
 });
